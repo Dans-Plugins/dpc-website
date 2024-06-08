2b133c41ac4eb908b0bb829786f2f1e69b1c9c8d
dmccoystephenson <dmccoystephenson@gmail.com>
2024-06-08 08:58:44 -0600

    Added environment variables for external links

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -3,15 +3,24 @@ import React, { useContext } from 'react';
 import { ColorModeToggleSwitch } from './ColorModeToggleSwitch';
 import { ColorModeContext } from '../utils/ColorModeContext';
 
+// function to get environment variable
+function getEnvVar(name: string): string {
+  const returnVar = process.env[name];
+  if (returnVar === '') {
+    console.error(`Environment variable ${name} not found!`);
+  }
+  return process.env[name] || '';
+}
+
 // declare each button
 const HomeButton = () => <Button color="inherit" href="/">Home</Button>
 const GuidesButton = () => <Button color="inherit" href="/guides">Guides</Button>
-const GitHubButton = () => <Button color="inherit" href="https://github.com/Dans-Plugins">GitHub</Button>
-const DiscordButton = () => <Button color="inherit" href="https://discord.gg/xXtuAQ2">Discord</Button>
-const PlaytestServerButton = () => <Button color="inherit" href="https://www.planetminecraft.com/server/dan-s-plugins-community-playtest-server/">Playtest Server</Button>
-const PatreonButton = () => <Button color="inherit" href="https://www.patreon.com/danthetechman">Patreon</Button>
-const LinkedInButton = () => <Button color="inherit" href="https://www.linkedin.com/company/dansplugins">LinkedIn</Button>
-const RPKitButton = () => <Button color="inherit" href="https://github.com/RP-Kit/RPKit">RPKit</Button>
+const GitHubButton = () => <Button color="inherit" href={getEnvVar('TOPBAR_DANSPLUGINS_GITHUB_ORGANIZATION_URL')}>GitHub</Button>
+const DiscordButton = () => <Button color="inherit" href={getEnvVar('TOPBAR_DISCORD_SERVER_URL')}>Discord</Button>
+const PlaytestServerButton = () => <Button color="inherit" href={getEnvVar('TOPBAR_PLAYTEST_SERVER_PMC_URL')}>Playtest Server</Button>
+const PatreonButton = () => <Button color="inherit" href={getEnvVar('TOPBAR_PATREON_PAGE_URL')}>Patreon</Button>
+const LinkedInButton = () => <Button color="inherit" href={getEnvVar('TOPBAR_LINKEDIN_PAGE_URL')}>LinkedIn</Button>
+const RPKitButton = () => <Button color="inherit" href={getEnvVar('TOPBAR_RPKIT_GITHUB_ORGANIZATION_URL')}>RPKit</Button>
 
 // declare react component for top bar
 const TopBar: React.FC = () => {
